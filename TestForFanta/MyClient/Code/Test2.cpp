e50e158640a63ef11f3eb8fde88c1509138b0d4d
HanUIn123 <gksyy12345@gmail.com>
2024-09-10 10:01:45 +0900

    Test2 7번 필터 들어갑니다~

---
+++ TestForFanta/MyClient/Code/Test2.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include "../Header/Test2.h"
+
+CTest2::CTest2()
+{
+}
+
+CTest2::~CTest2()
+{
+}
---
+++ TestForFanta/MyClient/Header/Test2.h
@@ -0,0 +1,8 @@
+#pragma once
+class CTest2
+{
+private:
+    explicit CTest2();
+    ~CTest2();
+};
+
